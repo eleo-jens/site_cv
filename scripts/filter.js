cac521af05dcf68b3d9483e3652e6f5a05b4b27d
Eleonore <eleonore.jens@gmail.com>
2022-11-15 17:40:45 +0100

    filter feature

--- scripts/filter.js
+++ scripts/filter.js
@@ -3,27 +3,23 @@ console.log(document.querySelector(".filter"));
 
 console.log(document.querySelector(".filter").id);
 
-document.querySelector(".filter").addEventListener('click', (event) => {
-    console.log("Hello");
-    console.log(event);
-})
+document.querySelectorAll(".filter").forEach(function (elem) {
+  elem.addEventListener("click", (event) => {
+    console.log(elem.id);
+    filter(elem.id);
+  });
+});
 
-
-let filter = function(){
-    document.getElementById("php").addEventListener('click', (event) =>
-    {
-        console.log("j'ai cliqué sur php");
-        // all: display none;
-        let toHide = document.getElementsByClassName("all");
-        for (let i = 0; i < toHide.length; i++){
-            toHide[i].style.display = "none";
-        }
-        let toShow = document.getElementsByClassName("php");
-        for (let i = 0; i < toShow.length; i++){
-            toShow[i].style.display = "block";
-        }
-        // php: display
-    
-    })
-}
-
+let filter = function (category) {
+    console.log(`J'ai cliqué sur ${category}`);
+    // all: display none;
+    let toHide = document.getElementsByClassName("all");
+    for (let i = 0; i < toHide.length; i++) {
+      toHide[i].style.display = "none";
+    }
+    // category: display
+    let toShow = document.getElementsByClassName(category);
+    for (let i = 0; i < toShow.length; i++) {
+      toShow[i].style.display = "block";
+    }
+};
