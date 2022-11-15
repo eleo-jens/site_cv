4a082c33092d80c4637e607414664a7fa811e368
Eleonore <eleonore.jens@gmail.com>
2022-11-15 18:43:28 +0100

    modal feature

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,25 +1,8 @@
-// Get the modal
-let modal = document.getElementById("myModal");
+const modalContainer = document.querySelector(".modal-container");
+const modalTriggers = document.querySelectorAll(".modal-trigger");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+modalTriggers.forEach(trigger => trigger.addEventListener("click", toggleModal));
 
-// Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+function toggleModal() {
+  modalContainer.classList.toggle("active");
 }
