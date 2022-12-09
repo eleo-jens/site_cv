ae73ec921a3c0bf5d7a30e4a15f6bf0453a0d7b0
Eleonore Stultjens <eleonore.jens@gmail.com>
2022-12-09 16:43:33 +0100

    fix burger click + scroll

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,9 +1,22 @@
 const burgerIcon = document.querySelector('.nav-toggler');
 const navigation = document.querySelector('nav');
+const menu_ul = document.querySelector('ul.menu');
+const menu_elem = document.querySelectorAll('ul.menu li');
+console.log(menu_elem);
+
 
 const toggleNav = function () {
     burgerIcon.classList.toggle("active");
     navigation.classList.toggle("active");
+    document.body.style.overflowY = "hidden";
 };
 
 burgerIcon.addEventListener("click", toggleNav);
+
+menu_elem.forEach(function(elem) {
+    elem.addEventListener("click", function () {
+        burgerIcon.classList.toggle("active");
+        navigation.classList.toggle("active");
+        document.body.style.overflowY = "scroll";
+    })
+});
