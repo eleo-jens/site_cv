1bf2f07f76c5d7b9fda52063322ab8f8e77413e1
Eleonore <eleonore.jens@gmail.com>
2022-12-11 20:39:54 +0100

    fix click icon theme

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,10 +1,11 @@
-const theme_toggle = document.querySelector(".theme");
-const visible_icon = theme_toggle.childNodes;
+const theme_toggle = document.querySelector(".theme i");
+// console.log(theme_toggle);
 
 theme_toggle.addEventListener("click", function () {
-    if (visible_icon[0].classList.contains("fa-moon") || visible_icon[0].classList.contains("fa-sun")) {
-        visible_icon[0].classList.toggle("fa-moon");
-        visible_icon[0].classList.toggle("fa-sun");
+
+    if (theme_toggle.classList.contains("fa-moon") || theme_toggle.classList.contains("fa-sun")) {
+        theme_toggle.classList.toggle("fa-moon");
+        theme_toggle.classList.toggle("fa-sun");
         document.body.classList.toggle("light");
         document.body.classList.toggle("dark");
     }
