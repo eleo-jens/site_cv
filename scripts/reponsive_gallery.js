7e1f01bbfb9b961e0dc1e6bfe5d8a05f8dc39190
Eleonore Stultjens <eleonore.jens@gmail.com>
2022-12-12 13:50:03 +0100

    essai fix modal responsive

--- scripts/reponsive_gallery.js
+++ scripts/reponsive_gallery.js
@@ -12,3 +12,25 @@
 // }
 
 // window.addEventListener("resize", redimension);
+window.addEventListener("resize", function () {
+    // if (window.matchMedia("(min-width: 480px)").matches) {
+    if (this.window.innerWidth > 577) {
+        // document.write('<script type="text/javascript" src="js/audioplayer.js"></script>');
+        if (!this.document.querySelector("script[src='./scripts/modal.js']")) {
+            let script = document.createElement('script');
+            script.type = "text/javascript";
+            script.src = "./scripts/modal.js";
+            script.setAttribute("defer", "");
+            console.log(script);
+            document.querySelector('head').appendChild(script);
+            
+        }
+    }
+    else {
+        if (this.document.querySelector("script[src='./scripts/modal.js']")) {
+            const head = document.querySelector("head");
+            console.log(head);
+            head.removeChild(this.document.querySelector("script[src='./scripts/modal.js']"));
+        }
+    }
+});
