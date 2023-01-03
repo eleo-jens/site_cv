695300e0170314eca7142a0a2cd8f22ba822e5bc
Eleonore Stultjens <eleonore.jens@gmail.com>
2023-01-03 16:42:03 +0100

    fix burger

--- scripts/reponsive_gallery.js
+++ scripts/reponsive_gallery.js
@@ -11,10 +11,40 @@
 //     }
 // }
 
+
+if (window.matchMedia("(min-width: 480px)").matches) {
+    // console.log("I'm bigger than 480px");
+/* La largeur minimum de l'affichage est 480 px inclus */
+// if (this.window.innerWidth > 577) {
+    // document.write('<script type="text/javascript" src="js/audioplayer.js"></script>');
+    if (!this.document.querySelector("script[src='./scripts/modal.js']")) {
+        let script = document.createElement('script');
+        script.type = "text/javascript";
+        script.src = "./scripts/modal.js";
+        script.setAttribute("defer", "");
+        console.log(script);
+        document.querySelector('head').appendChild(script);
+        
+    }
+}
+else {
+    /* L'affichage est inférieur à 480px de large */
+    if (this.document.querySelector("script[src='./scripts/modal.js']")) {
+        // console.log(document.querySelector("script[src='./scripts/modal.js']"));
+        $("script[src='./scripts/modal.js']").hide(0);
+        // const head = document.querySelector("head");
+        // console.log(head);
+        // head.removeChild(this.document.querySelector("script[src='./scripts/modal.js']"));
+    }
+}
+
+
 // window.addEventListener("resize", redimension);
 window.addEventListener("resize", function () {
-    // if (window.matchMedia("(min-width: 480px)").matches) {
-    if (this.window.innerWidth > 577) {
+    if (window.matchMedia("(min-width: 480px)").matches) {
+        // console.log("I'm bigger than 480px");
+    /* La largeur minimum de l'affichage est 480 px inclus */
+    // if (this.window.innerWidth > 577) {
         // document.write('<script type="text/javascript" src="js/audioplayer.js"></script>');
         if (!this.document.querySelector("script[src='./scripts/modal.js']")) {
             let script = document.createElement('script');
@@ -27,10 +57,20 @@ window.addEventListener("resize", function () {
         }
     }
     else {
+        /* L'affichage est inférieur à 480px de large */
         if (this.document.querySelector("script[src='./scripts/modal.js']")) {
-            const head = document.querySelector("head");
-            console.log(head);
-            head.removeChild(this.document.querySelector("script[src='./scripts/modal.js']"));
+            // console.log(document.querySelector("script[src='./scripts/modal.js']"));
+            $("script[src='./scripts/modal.js']").hide(0);
+            // const head = document.querySelector("head");
+            // console.log(head);
+            // head.removeChild(this.document.querySelector("script[src='./scripts/modal.js']"));
         }
     }
 });
+
+
+// if (window.matchMedia("(min-width: 600px)").matches) {
+    /* La largeur minimum de l'affichage est 600 px inclus */
+//   } else {
+    /* L'affichage est inférieur à 600px de large */
+//   }
