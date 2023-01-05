c5913ffa8ccea863d930fc1292e257670cf549ba
Eleonore Stultjens <eleonore.jens@gmail.com>
2023-01-05 16:08:36 +0100

    fix color logo

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,4 +1,6 @@
 const theme_toggle = document.querySelector(".theme i");
+const logo = document.querySelector("header>a>img");
+console.log(logo);
 // console.log(theme_toggle);
 
 theme_toggle.addEventListener("click", function () {
@@ -8,5 +10,11 @@ theme_toggle.addEventListener("click", function () {
         theme_toggle.classList.toggle("fa-sun");
         document.body.classList.toggle("light");
         document.body.classList.toggle("dark");
+    }
+    if(theme_toggle.classList.contains("fa-sun")){
+        logo.setAttribute("src","./assets/img/logoeleblanc.png");
+    }
+    else if(theme_toggle.classList.contains("fa-moon")) {
+        logo.setAttribute("src","./assets/img/logoele.png");
     }
 });
