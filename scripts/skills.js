5fefdc22f22d8d3f2b93ac3470f36dc8a60ebc4f
Eleonore Stultjens <eleonore.jens@gmail.com>
2023-02-01 14:16:37 +0100

    nb projects

--- scripts/skills.js
+++ scripts/skills.js
@@ -1,40 +1,40 @@
 // import Chart from '../node_modules/chart.js/auto';
 
-const graph = document.getElementById('myGraph');
+// const graph = document.getElementById('myGraph');
 
-const data = {
-  labels: [
-    'C#',
-    'PHP',
-    'JavaScript',
-    'CSS/Html',
-    'SQL',
-    'Angular', 
-    'ASP.Net',
-    'Symfony'
-  ],
-  datasets: [{
-    label: 'IT Skills %',
-    data: [70, 75, 60, 50, 50, 30, 60, 0],
-    backgroundColor: [
-      'rgb(255, 99, 132)',
-      'rgb(75, 192, 192)',
-      'rgb(255, 205, 86)',
-      'rgb(201, 203, 207)',
-      'rgb(54, 162, 235)',
-      'rgb(54, 0, 235)',
-      'rgb(1, 162, 1)',
-      'rgb(54, 0, 1)'
-    ]
-  }]
-};
+// const data = {
+//   labels: [
+//     'C#',
+//     'PHP',
+//     'JavaScript',
+//     'CSS/Html',
+//     'SQL',
+//     'Angular', 
+//     'ASP.Net',
+//     'Symfony'
+//   ],
+//   datasets: [{
+//     label: 'IT Skills %',
+//     data: [70, 75, 60, 50, 50, 30, 60, 0],
+//     backgroundColor: [
+//       'rgb(255, 99, 132)',
+//       'rgb(75, 192, 192)',
+//       'rgb(255, 205, 86)',
+//       'rgb(201, 203, 207)',
+//       'rgb(54, 162, 235)',
+//       'rgb(54, 0, 235)',
+//       'rgb(1, 162, 1)',
+//       'rgb(54, 0, 1)'
+//     ]
+//   }]
+// };
 
-new Chart(graph, 
-  {
-  type: 'polarArea',
-  data: data,
-  options: {}
-}); 
+// new Chart(graph, 
+//   {
+//   type: 'polarArea',
+//   data: data,
+//   options: {}
+// }); 
 
 /*BUBBLE GRAPH BUG**/
 
